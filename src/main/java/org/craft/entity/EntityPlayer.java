94b976328b325444c2c914d570814ec84d1bead2
Thog92 <thog92@hotmail.fr>
2014-09-21 20:07:40 +0200

    Change mouse click (now place and break block by block

--- src/main/java/org/craft/entity/EntityPlayer.java
+++ src/main/java/org/craft/entity/EntityPlayer.java
@@ -44,10 +44,14 @@ public class EntityPlayer extends Entity
         CollisionInfos infos = OurCraft.getOurCraft().getObjectInFront();
         if(infos.type == CollisionType.BLOCK)
         {
-            if(Mouse.isButtonDown(0))
-                getWorld().setBlock((int) infos.x + infos.side.getTranslationX(), (int) (infos.y) + infos.side.getTranslationY(), (int) (infos.z + infos.side.getTranslationZ()), Blocks.glass);
-            else if(Mouse.isButtonDown(1))
-                getWorld().setBlock((int) Math.round(infos.x), (int) Math.round(infos.y), (int) Math.round(infos.z), Blocks.air);
+            if(Mouse.next())
+            {
+                if ((Mouse.getEventButton() == 0) && (Mouse.getEventButtonState())) 
+                    getWorld().setBlock((int) Math.round(infos.x), (int) Math.round(infos.y), (int) Math.round(infos.z), Blocks.air);
+                else if ((Mouse.getEventButton() == 1) && (Mouse.getEventButtonState())) 
+                    getWorld().setBlock((int) infos.x + infos.side.getTranslationX(), (int) (infos.y) + infos.side.getTranslationY(), (int) (infos.z + infos.side.getTranslationZ()), Blocks.glass);
+            }
+           
         }
     }
 
