452b1a2da5bbc25a8b8f8f8df457e63fab8ae60b
jglrxavpok <jglrxavpok@gmail.com>
2014-10-23 22:03:33 +0200

    Added 10000% necessary code

--- src/main/java/org/craft/client/gui/GuiMainMenu.java
+++ src/main/java/org/craft/client/gui/GuiMainMenu.java
@@ -13,10 +13,27 @@ public class GuiMainMenu extends Gui
 {
 
     private ResourceLocation logoTexture = new ResourceLocation("ourcraft", "textures/logo.png");
+    private String           wittyText;
 
     public GuiMainMenu(OurCraft game)
     {
         super(game);
+        wittyText = "missigno";
+        ResourceLocation loc = new ResourceLocation("ourcraft", "text/splashes.txt");
+        if(oc.getAssetsLoader().doesResourceExists(loc))
+        {
+            try
+            {
+                AbstractResource res = oc.getAssetsLoader().getResource(loc);
+                String[] lines = new String(res.getData(), "UTF-8").split("\n");
+                int index = (int) (Math.random() * lines.length);
+                wittyText = lines[index];
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -25,6 +42,7 @@ public class GuiMainMenu extends Gui
         addWidget(new GuiButton(0, oc.getDisplayWidth() / 2 - 150, oc.getDisplayHeight() / 2, 300, 40, I18n.format("main.play.singleplayer"), getFontRenderer()));
         addWidget(new GuiButton(1, oc.getDisplayWidth() / 2 - 150, oc.getDisplayHeight() / 2 + 60, 300, 40, I18n.format("main.play.multiplayer"), getFontRenderer()));
         addWidget(new GuiButton(10, oc.getDisplayWidth() / 2 - 150, oc.getDisplayHeight() / 2 + 120, 300, 40, I18n.format("main.quit"), getFontRenderer()));
+
     }
 
     public void actionPerformed(GuiWidget widget)
@@ -61,6 +79,8 @@ public class GuiMainMenu extends Gui
         renderEngine.bindLocation(logoTexture);
         drawTexturedRect(renderEngine, oc.getDisplayWidth() / 2 - 217, 20, 433, 240, 0, 0, 1, 1);
         super.draw(mx, my, renderEngine);
+
+        getFontRenderer().drawShadowedString(wittyText, 0xFFC0C000, (int) (oc.getDisplayWidth() - getFontRenderer().getTextWidth(wittyText)), (int) (oc.getDisplayHeight() - getFontRenderer().getCharHeight('A')), renderEngine);
     }
 
     @Override
