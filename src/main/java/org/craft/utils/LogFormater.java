955ea9594ef262f58141ac9b44559266775a724a
Thog92 <thog92@hotmail.fr>
2014-11-18 19:23:31 +0100

    Change Log Format

--- src/main/java/org/craft/utils/LogFormater.java
+++ src/main/java/org/craft/utils/LogFormater.java
@@ -18,10 +18,8 @@ public class LogFormater extends Formatter
         StringBuilder stringbuilder = new StringBuilder();
         stringbuilder.append(this.dataformat.format(Long.valueOf(par1LogRecord
                 .getMillis())));
-        stringbuilder.append(" [Client]");
+        stringbuilder.append(" [Client/").append(par1LogRecord.getLevel().getName()).append("] ");
 
-        stringbuilder.append(" [").append(par1LogRecord.getLevel().getName())
-                .append("] ");
         stringbuilder.append(this.formatMessage(par1LogRecord));
         stringbuilder.append('\n');
         Throwable throwable = par1LogRecord.getThrown();
