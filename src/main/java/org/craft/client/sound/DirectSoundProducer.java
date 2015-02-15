20c37ba00b1e717fe89ecce421c979dee5551b22
Thog <thog92@hotmail.fr>
2015-02-15 18:47:18 +0100

    Remove SoundSystem (Sound Support removed temporary)

--- src/main/java/org/craft/client/sound/DirectSoundProducer.java
+++ src/main/java/org/craft/client/sound/DirectSoundProducer.java
@@ -4,40 +4,16 @@ import java.util.*;
 
 import com.google.common.collect.*;
 
-import de.cuina.fireandfuel.*;
-
 import org.craft.client.*;
 import org.craft.maths.*;
 import org.craft.sound.*;
-import org.craft.utils.*;
 import org.craft.world.*;
-
-import paulscode.sound.*;
-
 public class DirectSoundProducer implements IAudioHandler
 {
 
-    private SoundSystem        sndSystem;
-    private List<Sound>        playingSounds;
-    public static final String BACKGROUND_MUSIC = "backMusic";
-
-    public DirectSoundProducer()
-    {
-        try
-        {
-            playingSounds = Lists.newArrayList();
-            SoundSystemConfig.addLibrary(paulscode.sound.libraries.LibraryLWJGLOpenAL.class);
-            SoundSystemConfig.setCodec("ogg", paulscode.sound.codecs.CodecJOrbis.class);
-            SoundSystemConfig.setCodec("wav", paulscode.sound.codecs.CodecWav.class);
-            SoundSystemConfig.setCodec("mp3", CodecJLayerMP3.class);
-            SoundSystemConfig.setDefaultFadeDistance(100);
-        }
-        catch(Throwable throwable)
-        {
-            Log.error("Error linking with the LibraryOpenAL plug-in", throwable);
-        }
-        sndSystem = new SoundSystem();
-    }
+    public static final String      BACKGROUND_MUSIC = "backMusic";
+    //private SoundSystem        sndSystem;
+    private             List<Sound> playingSounds    = Lists.newArrayList();
 
     @Override
     public void playSound(String id, World w, float x, float y, float z)
@@ -48,10 +24,10 @@ public class DirectSoundProducer implements IAudioHandler
     @Override
     public void playSound(Sound sound)
     {
-        sndSystem.newStreamingSource(false, sound.getSourceName(), sound.getURL(), sound.getFileIdentifier(), false, sound.getPosX(), sound.getPosY(), sound.getPosZ(), 0, 0);
-        sndSystem.setVolume(sound.getSourceName(), sound.getVolume());
-        sndSystem.setPitch(sound.getSourceName(), sound.getPitch());
-        sndSystem.play(sound.getSourceName());
+        //sndSystem.newStreamingSource(false, sound.getSourceName(), sound.getURL(), sound.getFileIdentifier(), false, sound.getPosX(), sound.getPosY(), sound.getPosZ(), 0, 0);
+        //sndSystem.setVolume(sound.getSourceName(), sound.getVolume());
+        //sndSystem.setPitch(sound.getSourceName(), sound.getPitch());
+        //sndSystem.play(sound.getSourceName());
         playingSounds.add(sound);
     }
 
@@ -72,22 +48,15 @@ public class DirectSoundProducer implements IAudioHandler
 
     private void playMusicWithID(Music music, String id)
     {
-        sndSystem.newStreamingSource(true, id, music.getURL(), music.getFileIdentifier(), false, 0, 0, 0, SoundSystemConfig.ATTENUATION_ROLLOFF, 0);
-        sndSystem.setVolume(id, music.getVolume());
-        sndSystem.setPitch(id, music.getPitch());
-        sndSystem.play(id);
+        //sndSystem.newStreamingSource(true, id, music.getURL(), music.getFileIdentifier(), false, 0, 0, 0, SoundSystemConfig.ATTENUATION_ROLLOFF, 0);
+        //sndSystem.setVolume(id, music.getVolume());
+        //sndSystem.setPitch(id, music.getPitch());
+        //sndSystem.play(id);
     }
 
     public void setListenerOrientation(Quaternion q)
     {
-        float lookX = q.getForward().negative().getX();
-        float lookY = q.getForward().negative().getY();
-        float lookZ = q.getForward().negative().getZ();
 
-        float upX = q.getUp().getX();
-        float upY = q.getUp().getY();
-        float upZ = q.getUp().getZ();
-        sndSystem.setListenerOrientation(lookX, lookY, lookZ, upX, upY, upZ);
     }
 
     public void setListenerLocation(ILocatable loc)
@@ -95,10 +64,7 @@ public class DirectSoundProducer implements IAudioHandler
         setListenerLocation(loc.getPosX(), loc.getPosY(), loc.getPosZ());
     }
 
-    public void setListenerLocation(float x, float y, float z)
-    {
-        sndSystem.setListenerPosition(x, y, z);
-    }
+    public void setListenerLocation(float x, float y, float z) {}
 
     public void playBackgroundMusic(String string)
     {
@@ -112,37 +78,41 @@ public class DirectSoundProducer implements IAudioHandler
 
     public boolean isMusicPlaying()
     {
-        return sndSystem.playing(BACKGROUND_MUSIC);
+        return /*sndSystem.playing(BACKGROUND_MUSIC)*/ false;
     }
 
     public void setSoundVolume(float volume)
     {
         List<Sound> toRemove = Lists.newArrayList();
-        for(int i = 0; i < playingSounds.size(); i++ )
+        for(int i = 0; i < playingSounds.size(); i++)
         {
             Sound playing = playingSounds.get(i);
             if(playing == null)
+            {
                 continue;
-            sndSystem.setVolume(playing.getSourceName(), volume);
+            }
+            //sndSystem.setVolume(playing.getSourceName(), volume);
             if(!isPlaying(playing.getSourceName()))
+            {
                 toRemove.add(playing);
+            }
         }
-        playingSounds.removeAll(toRemove);
+        //playingSounds.removeAll(toRemove);
     }
 
     public boolean isPlaying(String sourceName)
     {
-        return sndSystem.playing(sourceName);
+        return false; //sndSystem.playing(sourceName);
     }
 
     public void setMusicVolume(float volume)
     {
-        sndSystem.setVolume(BACKGROUND_MUSIC, volume);
+        //sndSystem.setVolume(BACKGROUND_MUSIC, volume);
     }
 
     public void setMasterVolume(float volume)
     {
-        sndSystem.setMasterVolume(volume);
+        //sndSystem.setMasterVolume(volume);
     }
 
     @Override
@@ -153,6 +123,7 @@ public class DirectSoundProducer implements IAudioHandler
 
     public void cleanUp()
     {
-        sndSystem.cleanup();
+        //sndSystem.cleanup();
     }
+
 }
